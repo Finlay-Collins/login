aa3f11e503b52a2620faadb4936eea6a891e71d7
Finlay Collins <17ColliF@fulford.york.sch.uk>
2023-01-30 11:17:43 +0000

    yeee

---
+++ static/script.js
@@ -0,0 +1,28 @@
+let elements = {
+    username: document.getElementById("username"),
+    password: document.getElementById("password"),
+    hints: document.getElementById("hints")
+};
+
+function createUser() {
+
+};
+
+function reset() {
+
+};
+
+function login() {
+    presenceCheck(elements.password.value, "Please enter a password");
+    presenceCheck(elements.username.value, "Please enter a username");
+};
+
+function presenceCheck(input, message) {
+    if (input == "") {
+        elements.hints.innerText = message;
+    }
+
+    else {
+        elements.hints.innerText = "";
+    }
+};
